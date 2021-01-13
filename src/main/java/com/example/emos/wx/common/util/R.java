3a3622a157ce19d4922590f8b53ee036e033a083
HackBL <bojms45@gmail.com>
2021-01-13 00:12:33 -0500

    overloading error methods in R

--- src/main/java/com/example/emos/wx/common/util/R.java
+++ src/main/java/com/example/emos/wx/common/util/R.java
@@ -31,4 +31,19 @@ public class R extends HashMap<String, Object> {
     public static R ok() {
         return new R();
     }
+
+    public static R error(int code, String msg) {
+        R r = new R();
+        r.put("code", code);
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static R error(String msg) {
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, msg); // 500
+    }
+
+    public static R error() {
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, "未知异常，请联系管理员");
+    }
 }
