82ba81452477df31af3cf23460f9bdc0f978b468
HackBL <bojms45@gmail.com>
2021-01-14 23:44:10 -0500

    Prevent xss attack

---
+++ src/main/java/com/example/emos/wx/config/xss/XssHttpServletRequestWrapper.java
@@ -0,0 +1,11 @@
+package com.example.emos.wx.config.xss;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
+    public XssHttpServletRequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+}
