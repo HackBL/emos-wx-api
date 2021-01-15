f0a4dafa62d2da8729b9aa02a944add68009926a
HackBL <bojms45@gmail.com>
2021-01-15 02:55:37 -0500

    Request to wrapper

--- src/main/java/com/example/emos/wx/config/xss/XssFilter.java
+++ src/main/java/com/example/emos/wx/config/xss/XssFilter.java
@@ -1,8 +1,12 @@
 package com.example.emos.wx.config.xss;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+// all paths request will be intercepted
+@WebFilter(urlPatterns = "/*")
 public class XssFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -11,7 +15,9 @@ public class XssFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+        HttpServletRequest request = (HttpServletRequest) servletRequest;   // Cast
+        XssHttpServletRequestWrapper wrapper = new XssHttpServletRequestWrapper(request);
+        filterChain.doFilter(wrapper, servletResponse);
     }
 
     @Override
