75a50c966a4fedc7cc3986277f090fe11a7d841e
HackBL <bojms45@gmail.com>
2021-01-17 02:14:01 -0500

    encapsulate token for Shiro

---
+++ src/main/java/com/example/emos/wx/config/shiro/OAuth2Token.java
@@ -0,0 +1,21 @@
+package com.example.emos.wx.config.shiro;
+
+import org.apache.shiro.authc.AuthenticationToken;
+
+public class OAuth2Token implements AuthenticationToken {
+    private String token;
+
+    public OAuth2Token(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return token;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
+}
