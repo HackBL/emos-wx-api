90fdbbfccbb02f79d5470947623b5c22ef8a5db9
HackBL <bojms45@gmail.com>
2021-01-16 22:01:16 -0500

    Create tokens using userId, secret and expire

--- src/main/java/com/example/emos/wx/config/shiro/JwtUtil.java
+++ src/main/java/com/example/emos/wx/config/shiro/JwtUtil.java
@@ -1,8 +1,15 @@
 package com.example.emos.wx.config.shiro;
 
+import cn.hutool.core.date.DateField;
+import cn.hutool.core.date.DateUtil;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 @Slf4j
@@ -12,4 +19,16 @@ public class JwtUtil {
 
     @Value("${emos.jwt.expire}")
     private int expire;
+
+    // generate Tokens with userId & secret & expire
+    public String createToken(int userId) {
+        // shift expiration date from cur date
+        Date date = DateUtil.offset(new Date(), DateField.DAY_OF_YEAR, expire).toJdkDate();
+        // encode secret using algorithm
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        // using inner class to generate tokens
+        JWTCreator.Builder builder= JWT.create();
+        return builder.withClaim("userId", userId).withExpiresAt(date).sign(algorithm);
+    }
+
 }
