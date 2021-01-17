974042da85844eae758631bc99c3398b88771bcd
HackBL <bojms45@gmail.com>
2021-01-16 21:22:32 -0500

    Create JwtUtil

---
+++ src/main/java/com/example/emos/wx/config/shiro/JwtUtil.java
@@ -0,0 +1,15 @@
+package com.example.emos.wx.config.shiro;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class JwtUtil {
+    @Value("${emos.jwt.secret}")
+    private String secret;
+
+    @Value("${emos.jwt.expire}")
+    private int expire;
+}
