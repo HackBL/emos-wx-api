c67c40c8633a5b5e687bba4adc035e747b774ecb
HackBL <bojms45@gmail.com>
2021-01-18 14:20:39 -0500

    support method to get token from request

--- src/main/java/com/example/emos/wx/config/shiro/OAuth2Filter.java
+++ src/main/java/com/example/emos/wx/config/shiro/OAuth2Filter.java
@@ -1,5 +1,6 @@
 package com.example.emos.wx.config.shiro;
 
+import cn.hutool.core.util.StrUtil;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 @Component
 @Scope("prototype")     // 在springboot中属于多例java类， 非单例类
@@ -35,5 +37,15 @@ public class OAuth2Filter extends AuthenticatingFilter {
     @Override
     protected boolean onAccessDenied(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
         return false;
+    }
+
+    private String getRequestToken(HttpServletRequest request) {
+        // get token from request header
+        String token = request.getHeader("token");
+        // get token from request body
+        if (StrUtil.isBlank(token)) {
+            token = request.getParameter("token");
+        }
+        return token;
     }
 }
