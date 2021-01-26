8ecb50abcefea43ebfd55fbb1ac991e719fbdc11
HackBL <bojms45@gmail.com>
2021-01-26 01:38:24 -0500

    Implement simplified exceptions

--- src/main/java/com/example/emos/wx/config/ExceptionAdvice.java
+++ src/main/java/com/example/emos/wx/config/ExceptionAdvice.java
@@ -1,9 +1,36 @@
 package com.example.emos.wx.config;
 
+import com.example.emos.wx.exception.EmosException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionAdvice {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String ExceptionHandler(Exception e) {
+        log.error("执行异常", e);
+        if (e instanceof MethodArgumentNotValidException) {
+            MethodArgumentNotValidException exception = (MethodArgumentNotValidException) e;
+            return exception.getBindingResult().getFieldError().getDefaultMessage();
+        }
+        else if (e instanceof EmosException) {
+            EmosException exception = (EmosException) e;
+            return exception.getMsg();
+        }
+        else if (e instanceof UnauthorizedException) {
+            return "你不具备相关权限";
+        }
+        else {
+            return "后端执行异常";
+        }
+    }
 }
