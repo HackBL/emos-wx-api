ae4fd4af8bc1064d2e05c31ec797a115c8862099
HackBL <bojms45@gmail.com>
2021-01-30 22:11:42 -0500

    Get openID by temporary code passing by WeChat user

--- src/main/java/com/example/emos/wx/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/emos/wx/service/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.emos.wx.service.impl;
 
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.example.emos.wx.db.dao.TbUserDao;
 import com.example.emos.wx.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+
 
 @Service
 @Slf4j
@@ -21,5 +28,23 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private TbUserDao userDao;
+
+    private String getOpenId(String code) {
+        String url = "https://api.weixin.qq.com/sns/jscode2session";
+        HashMap map = new HashMap();
+        map.put("appid", appId);
+        map.put("secret", appSecret);
+        map.put("js_code", code);
+        map.put("grant_type", "authorization_code");
+
+        String response = HttpUtil.post(url, map);
+        JSONObject json = JSONUtil.parseObj(response);
+        String openId = json.getStr("openid");
+
+        if (openId == null || openId.length() == 0) {
+            throw new RuntimeException("临时登陆凭证错误");
+        }
+        return openId;
+    }
 
 }
