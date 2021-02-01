4674d3ba9bca0dcf364bf090b34434ee92a2812f
HackBL <bojms45@gmail.com>
2021-02-01 00:23:47 -0500

    process register request

--- src/main/java/com/example/emos/wx/controller/userController.java
+++ src/main/java/com/example/emos/wx/controller/userController.java
@@ -1,14 +1,21 @@
 package com.example.emos.wx.controller;
 
+import com.example.emos.wx.common.util.R;
 import com.example.emos.wx.config.shiro.JwtUtil;
+import com.example.emos.wx.controller.form.RegisterForm;
 import com.example.emos.wx.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -27,6 +34,24 @@ public class userController {
     @Value("${emos.jwt.cache-expire}")
     private int cacheExpire;
 
+
+    /**
+     *  Process register request
+     * */
+    @PostMapping("/register")
+    @ApiOperation("注册用户")
+    public R register(@Valid @RequestBody RegisterForm form) {
+        int id = userService.registerUser(form.getRegisterCode(), form.getCode(), form.getNickname(), form.getPhoto());
+        String token = jwtUtil.createToken(id);
+        Set<String> permsSet = userService.searchUserPermissions(id);
+        saveCacheToken(token, id);
+
+        return R.ok("用户注册成功").put("token", token).put("permission", permsSet);
+    }
+
+    /**
+     *  Save token into Redis
+     * */
     private void saveCacheToken(String token, int userId) {
         redisTemplate.opsForValue().set(token, userId+"", cacheExpire, TimeUnit.DAYS);
     }
