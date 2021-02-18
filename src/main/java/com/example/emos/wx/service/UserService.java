2dd3f604be4f32a944fcab50bec9f1176e1f5ffd
HackBL <bojms45@gmail.com>
2021-02-18 02:15:40 -0500

    Rename method

--- src/main/java/com/example/emos/wx/service/UserService.java
+++ src/main/java/com/example/emos/wx/service/UserService.java
@@ -13,6 +13,6 @@ public interface UserService {
 
     public TbUser searchById(int userId);
 
-    public String searchUserHireDate(int userId);
+    public String searchUserHiredate(int userId);
 
 }
